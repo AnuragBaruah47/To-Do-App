6e3f08b1a657bb929e4d4b7e11e409603d190b85
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-02-16 22:52:42 +0530

    Clear button added and modified and list button added

--- script.js
+++ script.js
@@ -5,33 +5,51 @@ const deleteButton = document.querySelector(".delete");
 const listButton = document.querySelector(".list");
 const clearButton = document.querySelector(".clear");
 const display = document.querySelector(".display");
-const myArr = [];
+const orderList = document.querySelector(".ol");
+let myArr = [];
+let myArrValues = 0;
 
-inputTag.addEventListener("input", function () {
+inputTag.addEventListener("input", function () {});
+addButton.addEventListener("click", function () {
+  let inputValue = inputTag.value;
+  myArr.push(inputValue);
+  if (inputValue !== "") {
+    let i = myArr.length;
+    do {
+      const h3 = document.createElement("h3");
+      h3.textContent = `Task Added "${inputTag.value}"`;
+      display.appendChild(h3);
+      i++;
+      inputTag.value = "";
+      // console.log(myArr);
+      setTimeout(function () {
+        h3.textContent = ``;
+        display.appendChild(h3);
+      }, 3000);
+      // console.log(h3.textContent);
+    } while (i <= myArr.length);
+  }
 });
-addButton.addEventListener("click",function(){
-    let inputValue = inputTag.value;
-    myArr.push(inputValue);
-    if(inputValue!==""){
-        let i=myArr.length
-    do{
-        const h3=document.createElement("h3") 
-        h3.textContent=`Task Added "${inputTag.value}"`
-        display.appendChild(h3)
-        i++
-        inputTag.value=""
-        console.log(myArr); 
+
+listButton.addEventListener("click", function () {
+  orderList.innerHTML = "";
+  for (const values of myArr) {
+    let myArrValues = values;
+    const li = document.createElement("li");
+    li.textContent = `${myArrValues}`;
+    orderList.appendChild(li);
+  }
+});
+
+clearButton.addEventListener("click", function () {
+  orderList.innerHTML = "";
+});
+
+deleteButton.addEventListener("click",function(){
+  for (const values of myArr) {
+    if(inputTag.value===values){
+      myArr.splice(Array[values])
     }
-    while(i<=myArr.length)
-}})
-
-listButton.addEventListener("click",function(){
-    let inputValue = inputTag.value;
-        const h3=document.createElement("h3") 
-        h3.textContent=`${inputValue}`
-        display.appendChild(h3) 
-})
-
-clearButton.addEventListener("click",function(){
-    display.textContent=""
+  }
+  inputTag.value = "";
 })
