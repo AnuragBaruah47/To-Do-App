b09d6daf3d73532aa48ddc7cd7163629576c6666
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-02-21 06:15:10 +0530

    Almost done css and minor changes remains

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const listButton = document.querySelector(".list");
 const clearButton = document.querySelector(".clear");
 const display = document.querySelector(".display");
 const orderList = document.querySelector(".ol");
+const toggleButton=document.querySelector(".theme-checkbox")
 let myArr = [];
 let myArrValues = 0;
 
 inputTag.addEventListener("input", function () {});
 addButton.addEventListener("click", function () {
   let inputValue = inputTag.value;
-  myArr.push(inputValue);
-  if (inputValue !== "") {
-    let i = myArr.length;
-    do {
+  if (inputValue.trim() !== "") {
+    for(let i=myArr.length;i<=myArr.length;i++){
+      myArr.push(inputValue);
       const h3 = document.createElement("h3");
       h3.textContent = `Task Added "${inputTag.value}"`;
       display.appendChild(h3);
@@ -27,29 +27,31 @@ addButton.addEventListener("click", function () {
         display.appendChild(h3);
       }, 3000);
       // console.log(h3.textContent);
-    } while (i <= myArr.length);
-  }
+    }
+}});
+
+clearButton.addEventListener("click", function () {
+  orderList.innerHTML = ""; 
 });
+
+deleteButton.addEventListener("click",function(){
+  if(inputTag.value.trim()!==""){
+    orderList.innerHTML=`<h1>Do you Want To delete<h2/>`
+  const deleteValue=myArr.indexOf(inputTag.value)
+  myArr.splice(deleteValue,1)
+  inputTag.value = "";
+}})
 
 listButton.addEventListener("click", function () {
   orderList.innerHTML = "";
-  for (const values of myArr) {
+  for (const values of myArr) {  
     let myArrValues = values;
     const li = document.createElement("li");
     li.textContent = `${myArrValues}`;
     orderList.appendChild(li);
   }
-});
+}); 
 
-clearButton.addEventListener("click", function () {
-  orderList.innerHTML = "";
-});
-
-deleteButton.addEventListener("click",function(){
-  for (const values of myArr) {
-    if(inputTag.value===values){
-      myArr.splice(Array[values])
-    }
-  }
-  inputTag.value = "";
+toggleButton.addEventListener("click",function(){
+  
 })
