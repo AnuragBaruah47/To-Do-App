8022146dcaba14d0d6a5ce888c830ee3aa868eb6
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-02-21 12:12:50 +0530

    local storage added

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ const addButton = document.querySelector(".add");
 const deleteButton = document.querySelector(".delete");
 const listButton = document.querySelector(".list");
 const clearButton = document.querySelector(".clear");
+const bulkDeleteButton = document.querySelector(".deleteall");
 const display = document.querySelector(".display");
 const orderList = document.querySelector(".ol");
-const toggleButton=document.querySelector(".theme-checkbox")
+const toggleButton = document.querySelector(".theme-checkbox");
 let myArr = [];
 let myArrValues = 0;
 
-inputTag.addEventListener("input", function () {});
-addButton.addEventListener("click", function () {
-  let inputValue = inputTag.value;
-  if (inputValue.trim() !== "") {
-    for(let i=myArr.length;i<=myArr.length;i++){
+addEventListener("DOMContentLoaded", () => {
+  inputTag.addEventListener("input", function () {});
+  addButton.addEventListener("click", function () {
+    let inputValue = inputTag.value;
+    if (inputValue.trim() !== "") {
       myArr.push(inputValue);
+      const localArray = JSON.stringify(myArr);
+      localStorage.setItem("1", localArray);
       const h3 = document.createElement("h3");
       h3.textContent = `Task Added "${inputTag.value}"`;
       display.appendChild(h3);
-      i++;
       inputTag.value = "";
       // console.log(myArr);
       setTimeout(function () {
@@ -28,49 +30,88 @@ addButton.addEventListener("click", function () {
       }, 3000);
       // console.log(h3.textContent);
     }
-}});
+  });
 
-clearButton.addEventListener("click", function () {
-  orderList.innerHTML = ""; 
+  clearButton.addEventListener("click", function () {
+    orderList.innerHTML = "";
+  });
+
+  deleteButton.addEventListener("click", function () {
+    let inputValue = inputTag.value;
+    console.log(inputValue);
+
+    if (inputTag.value.trim() !== "") {
+      orderList.innerHTML = `<h1>Do you Want To delete<h2/>`;
+      const div = document.createElement("div");
+      display.appendChild(div);
+      const button1 = document.createElement("button");
+      const button2 = document.createElement("button");
+      button1.textContent = "Yes";
+      button2.textContent = "No";
+      div.appendChild(button1);
+      div.appendChild(button2);
+      button2.addEventListener("click", () => {
+        orderList.innerHTML = "";
+        div.innerHTML = "";
+      });
+      button1.addEventListener("click", () => {
+        const item = localStorage.getItem("1");
+        const arrayitem = JSON.parse(item);
+        const deleteValue = arrayitem.indexOf(inputValue);
+        myArr.splice(deleteValue, 1);
+        const localArray = JSON.stringify(myArr);
+        localStorage.setItem("1", localArray);
+        orderList.innerHTML = "Your Selected Value Has Been Removed";
+        div.innerHTML = "";
+        setTimeout(function () {
+          orderList.innerHTML = "";
+        }, 2000);
+      });
+      inputTag.value = "";
+    }
+  });
+
+  listButton.addEventListener("click", function () {
+    orderList.innerHTML = "";
+    const input = document.createElement("input");
+    input.value = "checkbox";
+    const item = localStorage.getItem("1");
+    const arrayitem = JSON.parse(item);
+    if (arrayitem.length >= 1) {
+      for (const values of arrayitem) {
+        let myArrValues = values;
+        const li = document.createElement("li");
+        li.appendChild(input);
+        li.textContent = `${myArrValues}`;
+        orderList.appendChild(li);
+      }
+    }
+  });
+
+  bulkDeleteButton.addEventListener("click", function () {
+    orderList.innerHTML = `<h1>Do you Want To delete<h2/>`;
+    const div = document.createElement("div");
+    display.appendChild(div);
+    const button1 = document.createElement("button");
+    const button2 = document.createElement("button");
+    button1.textContent = "Yes";
+    button2.textContent = "No";
+    div.appendChild(button1);
+    div.appendChild(button2);
+    button1.addEventListener("click", () => {
+      myArr = [];
+      orderList.innerHTML = "";
+      localStorage.clear();
+      orderList.innerHTML = "Your Task Has Been Deleted";
+      div.innerHTML = "";
+      setTimeout(function () {
+        orderList.innerHTML = "";
+      }, 2000);
+    });
+    inputTag.value = "";
+    button2.addEventListener("click", () => {
+      orderList.innerHTML = "";
+      div.innerHTML = "";
+    });
+  });
 });
-
-deleteButton.addEventListener("click",function(){
-  if(inputTag.value.trim()!==""){
-    orderList.innerHTML=`<h1>Do you Want To delete<h2/>`
-   const div=document.createElement("div")
-   display.appendChild(div)
-   const button1=document.createElement("button")
-   const button2=document.createElement("button")
-   button1.textContent="Yes"
-   button2.textContent="No"
-   div.appendChild(button1)
-   div.appendChild(button2)
-   button2.addEventListener("click",()=>{
-       orderList.innerHTML = ""
-           div.innerHTML=""
-   })
-   button1.addEventListener("click",()=>{
-    const deleteValue=myArr.indexOf(inputTag.value)
-    myArr.splice(deleteValue,1)
-    orderList.innerHTML = "Your Selected Value Has Been Removed";
-    div.innerHTML=""
-    setTimeout(function(){
-        orderList.innerHTML = ""
-    },2000)
-   })
-  inputTag.value = "";
-}})
-
-listButton.addEventListener("click", function () {
-  orderList.innerHTML = "";
-  for (const values of myArr) {  
-    let myArrValues = values;
-    const li = document.createElement("li");
-    li.textContent = `${myArrValues}`;
-    orderList.appendChild(li);
-  }
-}); 
-
-toggleButton.addEventListener("click",function(){
-  
-})
